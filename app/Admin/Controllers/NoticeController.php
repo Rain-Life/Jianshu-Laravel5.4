610c2eb7bb8cf0bf4b40cf8f4b683c2000f2070a
陈兴生 <chenxingsheng@51shebao.com>
2020-02-16 15:45:52 +0800

    通知队里实现

--- app/Admin/Controllers/NoticeController.php
+++ app/Admin/Controllers/NoticeController.php
@@ -22,7 +22,8 @@ class NoticeController extends Controller
            'content' => 'required|string'
         ]);
 
-        \App\Notice::create(request(['title', 'content']));
+        $notice = \App\Notice::create(request(['title', 'content']));
+        $this->dispatch(new \App\Jobs\SendMessage($notice));
 
         return redirect('/admin/notices');
     }
